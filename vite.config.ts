a2a78b3758081c4fe77eb0a7d20980676c187dc6
abbasfasel2012-source <abbasfasel2012@gmail.com>
2026-08-25 09:12:53 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   vite: {
     build: {
-      minify: "terser",
       sourcemap: false,
       rollupOptions: {
         output: {
